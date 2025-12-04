622433fe2d515cbf93463fd61416164adcd6da5c
Karl Andreas Mätlik <lihtsaltkasuta@gmail.com>
2025-12-04 08:21:18 +0200

    Another test build

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+  SPOTIFY_REDIRECT_URL: process.env.SPOTIFY_REDIRECT_URL,
+  SPOTIFY_REDIRECT_PORT: process.env.SPOTIFY_REDIRECT_PORT,
+};
